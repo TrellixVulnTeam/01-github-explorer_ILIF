5b6d97b7a565e7476983cd64f724a8ee86c8909e
Welber33 <jwwork21@gmail.com>
2021-09-23 11:11:11 -0300

    Imutabilidade no React e Fast Refresh no Webpack

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,5 +1,16 @@
 import { useState } from 'react';
 
+/**
+ * Imutabilidade
+ * 
+ * usuarios = ['diego3g', 'dieegosf', 'danileao']
+ * 
+ * usuarios.push('rafacamarda') => essa forma adiciona um novo usuário porém ta alterando diretamente a variável usuários, ou seja, causando uma mutação na variável
+ * 
+ * novoUsuarios = [...usuarios, 'rafacamarda'] => será utilizado esse tipo de conceito ao longo do projeto
+ *  
+ */
+
 export function Counter() {
     const [counter, setCounter] = useState(0);
     function increment() {
@@ -9,7 +20,9 @@ export function Counter() {
     return (
         <div>
             <h2>{counter}</h2>
-            <button type="button" onClick={increment}>Increment</button>
+            <button type="button" onClick={increment}>
+                Increment
+            </button>
         </div>
     );
 }
